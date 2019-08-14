436f4d8d65ba754a538545e717b9c9945ebf1aab
Nermin Ceric <nerceric@gmail.com>
2019-08-13 22:28:09 -0500

    added buttons for the search function

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
+
 const undead = ["skeletor", "kelthuzad", "lich king", "zombie", "freddy krueger", ];
 
 
-function renderButton() {
+function summonUndead() {
+    const smnUD = this.getAttribute("data-name");
+    const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + smnUD + "&api_key=VMHWOaSRDKW6OipJVWPzNh9sxy5QxfSe&limit=10&rating=G";
 
-document.getElementById("buttons").empty();
-
-for (let i = 0; i < undead.length; i++) {
-
-    const newOne = ("<button class='btn btn-info'>");
-     newOne.addclass("deadButton");
-     
+    fetch(queryURL)
+    .then(function (response) {
+return response.json();
+    }).then(function(responseJson) {
+        
+        
+        
+    });
 }
 
-}
+function renderButtons() {
+
+    document.getElementById("buttons").innerHTML = "";
+
+    for (let i=0; i < undead.length; i++) {
+
+        const necro = document.createElement("button");
+        necro.classList.add("ud");
+        necro.setAttribute("data-name", undead[i]);
+        necro.innerHTML = undead[i];
+
+        document.getElementById("buttons").append(necro);
+
+        necro.addEventListener("click", summonUndead);
+    }
+
+    }
+
+
+    document.getElementById("addUndead").addEventListener("click", function(event) {
+event.preventDefault();
+
+const smnUD = document.getElementById("addUndead").value.trim();
+
+undead.push(smnUD);
+console.log(undead);
+
+renderButtons();
+});
+
+renderButtons();
